1785c2620fb0e8c37744f0fbaf4bf0b166b63c9a
Rinzler <119778544+TheRinzler65@users.noreply.github.com>
2024-05-25 13:45:21 +0200

    13.1.1 en Français

---
+++ sysmodules/pxi/build/MyThread.d
@@ -0,0 +1,12 @@
+MyThread.o: \
+ C:/Users/leo/Downloads/Luma3DS-French/sysmodules/pxi/source/MyThread.c \
+ C:/Users/leo/Downloads/Luma3DS-French/sysmodules/pxi/source/MyThread.h \
+ C:/devkitPro/libctru/include/3ds/types.h \
+ C:/devkitPro/libctru/include/3ds/result.h \
+ C:/devkitPro/libctru/include/3ds/svc.h \
+ C:/devkitPro/libctru/include/3ds/synchronization.h
+C:/Users/leo/Downloads/Luma3DS-French/sysmodules/pxi/source/MyThread.h:
+C:/devkitPro/libctru/include/3ds/types.h:
+C:/devkitPro/libctru/include/3ds/result.h:
+C:/devkitPro/libctru/include/3ds/svc.h:
+C:/devkitPro/libctru/include/3ds/synchronization.h:
